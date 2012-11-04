b830f76af69c4810b0d60ed5dd60774917880682
Dragos Dascalita Haut <ddragosd@gmail.com>
2012-11-04 20:20:28 +0200

    initial commit of the new version of the flash video player. This commit contains the customer plugin developed for our CDN ( StrobeCDNPlugin), the OSMF 2.0 framework, and the StrobeMediaPlayer currently used. These last 2 projects have been downloaded from the internet.

---
+++ src/main/java/org/mediacenter/content/album/AlbumService.java
@@ -0,0 +1,12 @@
+package org.mediacenter.content.album;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ddascal
+ * Date: 10/15/12
+ * Time: 12:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlbumService
+{
+}
