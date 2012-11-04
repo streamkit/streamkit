62954d13c80ad7203c82d16d36df800ff388e727
Dragos Dascalita Haut <ddragosd@gmail.com>
2012-11-04 20:20:28 +0200

    initial commit of the new version of the flash video player. This commit contains the customer plugin developed for our CDN ( StrobeCDNPlugin), the OSMF 2.0 framework, and the StrobeMediaPlayer currently used. These last 2 projects have been downloaded from the internet.

---
+++ src/main/java/org/mediacenter/resource/MediaCenterResourceConstants.java
@@ -0,0 +1,12 @@
+package org.mediacenter.resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ddascal
+ * Date: 10/14/12
+ * Time: 10:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MediaCenterResourceConstants
+{
+}
