b399342d20024ef0a1f498e611b114beedfd6590
Dragos Dascalita Haut <ddragosd@gmail.com>
2013-03-07 23:40:43 +0100

    CMS-90 - Update query to sort by "created" field instead of "jcr:created" - worked on postprocessing to update post processors with a new API to call them

--- src/main/java/org/mediacenter/content/post/processor/CreateVodContentProcessor.java
+++ src/main/java/org/mediacenter/content/post/processor/CreateVodContentProcessor.java
@@ -1,11 +1,13 @@
 package org.mediacenter.content.post.processor;
 
 import java.text.Normalizer;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.nodetype.NodeType;
@@ -47,45 +49,50 @@ public class CreateVodContentProcessor extends EditVodProcessorBase implements S
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
-    protected void doProcess(SlingHttpServletRequest request, List<Modification> changes) throws Exception {
+    protected void doProcess(SlingHttpServletRequest request, List<Modification> changes) throws Exception
+    {
         super.doProcess(request, changes);
         validateLocation(request, changes);
     }
 
     /**
      * Method used to handle international characters and also to replace spaces with "_".
-     *
+     * <p/>
      * Algorithm for Node Name Creation
-
+     * <p/>
      * If request is posted with an URL ending in slash / or slash-star /*, the SlingPostServlet derives a name for the
      * node to be created upon the request applying the following algorithm:
-
+     * <p/>
      * If a :name parameter is supplied, the (first) value of this parameter is used unmodified as the name for the new node.
      * If the name is illegally formed with respect to JCR name requirements, an exception will be thrown when trying
      * to create the node.
-     *
+     * <p/>
      * The assumption with the :name parameter is, that the caller knows what he (or she) is supplying and should get
      * the exact result if possible.
-     *
+     * <p/>
      * Otherwise if a :nameHint parameter is supplied, the (first) value of this parameter is used to generate the node name.
      * A name filtering is applied to this hint to ensure a valid JCR node name.
-     *
+     * <p/>
      * Otherwise a series of request parameters supplied to set content is inspected for a possible name.
      * The list of the names of these parameter is configurable with the SlingPostServlet and defaults ot
      * [ title, jcr:title, name, description, jcr:description, abstract ]. The first request parameter with a non-empty
      * value is used and filtered to get the valid JCR name.
-     *
+     * <p/>
      * Otherwise an ever increasing auto generated number is used. Filtering is also applied to this numeric name.
-     *
+     * <p/>
      * To read more go to: http://sling.apache.org/site/manipulating-content-the-slingpostservlet-servletspost.html
      * -------------------
      * NOTE : IN PROGRESS
      * -------------------
+     *
      * @param request
      * @param changes
+     *
      * @throws Exception
      */
-    private void validateLocation(SlingHttpServletRequest request, List<Modification> changes) throws Exception {
+
+    private void validateLocation(SlingHttpServletRequest request, List<Modification> changes) throws Exception
+    {
         Session session = request.getResourceResolver().adaptTo(Session.class);
         /*
         Node n = session.getNode(request.getResource().getPath());
@@ -101,7 +108,8 @@ public class CreateVodContentProcessor extends EditVodProcessorBase implements S
         */
     }
 
-    private String getNormalizedName( String name ) {
+    private String getNormalizedName(String name)
+    {
         String nfdNormalizedString = Normalizer.normalize(name, Normalizer.Form.NFD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
         return pattern.matcher(nfdNormalizedString).replaceAll("");
